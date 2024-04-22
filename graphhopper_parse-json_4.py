4ef893625f430309ee29e792f4fd6869899e8f20
Dubois Renan <renan.dubois@epitech.eu>
2024-04-22 12:19:51 +0900

    End of the 4.9.2 lab

--- graphhopper_parse-json_4.py
+++ graphhopper_parse-json_4.py
@@ -42,6 +42,20 @@ def geocoding (location, key):
         print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"])
  return json_status,lat,lng,new_loc 
 while True:
+ print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+ print("Vehicle profiles available on Graphhopper:")
+ print("+++++++++++++++++++++++++++++++++++++++++++++")
+ print("car, bike, foot")
+ print("+++++++++++++++++++++++++++++++++++++++++++++")
+ profile=["car", "bike", "foot"]
+ vehicle = input("Enter a vehicle profile from the list above: ")
+ if vehicle == "quit" or vehicle == "q":
+    break
+ elif vehicle in profile:
+    vehicle = vehicle
+ else:
+    vehicle = "car"
+    print("No valid vehicle profile was entered. Using the car profile.") 
  loc1 = input("Starting Location: ")
  if loc1 == "quit" or loc1 == "q":
     break
@@ -55,8 +69,28 @@ while True:
  if orig[0] == 200 and dest[0] == 200: 
     op="&point="+str(orig[1])+"%2C"+str(orig[2])
     dp="&point="+str(dest[1])+"%2C"+str(dest[2])
-    paths_url = route_url + urllib.parse.urlencode({"key":key}) + op + dp
+    paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp 
     paths_status = requests.get(paths_url).status_code
     paths_data = requests.get(paths_url).json()
     print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url) 
-    print(dest)
+    print("=================================================")
+    print("Directions from " + orig[3] + " to " + dest[3] + " by " + vehicle)
+    print("=================================================")
+    if paths_status == 200:
+        miles = (paths_data["paths"][0]["distance"])/1000/1.61
+        km = (paths_data["paths"][0]["distance"])/1000
+        sec = int(paths_data["paths"][0]["time"]/1000%60)
+        min = int(paths_data["paths"][0]["time"]/1000/60%60)
+        hr = int(paths_data["paths"][0]["time"]/1000/60/60) 
+        print("Distance Traveled: {0:.1f} miles / {1:.1f} km".format(miles, km)) 
+        print("Trip Duration: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
+        print("=================================================")
+        for each in range(len(paths_data["paths"][0]["instructions"])):
+            path = paths_data["paths"][0]["instructions"][each]["text"]
+            distance = paths_data["paths"][0]["instructions"][each]["distance"]
+            print("{0} ( {1:.1f} km / {2:.1f} miles )".format(path, distance/1000,distance/1000/1.61))
+            print("=================================================")
+    else:
+        print("Error message: " + paths_data["message"])
+        print("*************************************************")
+    print("=================================================")
