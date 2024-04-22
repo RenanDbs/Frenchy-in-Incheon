3d469db01f3784f7b406d7c9e9924998a0330992
Dubois Renan <renan.dubois@epitech.eu>
2024-04-22 14:48:30 +0900

    Add the posibility to display waypoints on the html map

--- graphhopper_parse-json_4.py
+++ graphhopper_parse-json_4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cebcec1fd727fee83d2f927a0ab90adc91a6e3e1ae6bf9693924ede9ac7f269
-size 7147
+oid sha256:df4f1275b3158266f21fd797ac512dcdd036352e3c7e6db890d1b297ae7b3972
+size 7254
