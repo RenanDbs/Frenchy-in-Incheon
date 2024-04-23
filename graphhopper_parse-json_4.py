cf4fc878abb7d56c418e2e04b8a476b555d60dfa
JulianNDENDE <julian.ndende@epitech.eu>
2024-04-23 12:10:47 +0900

    Add some comments

--- graphhopper_parse-json_4.py
+++ graphhopper_parse-json_4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8fca00f8540e343ab9de11aa01c8f7b711249fa6e8efb29c14795665eb146f1
-size 10162
+oid sha256:19228dc047cdb72f912b6ed4435020a6f0b3f3b103a2a027c61bb9c9e9e09ad9
+size 10453
